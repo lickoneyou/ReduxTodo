a2a2d6af174ed7797b0eca529eddc533d92994f0
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-12-22 13:57:44 +0300

    completed localSrorage

--- src/components/Main/Btn/Btn.jsx
+++ src/components/Main/Btn/Btn.jsx
@@ -3,17 +3,28 @@ import Button from "@mui/joy/Button";
 import Add from "@mui/icons-material/Add";
 import styles from "./Btn.module.css";
 import { useSelector } from "react-redux";
+import randomId from "random-id";
 
 const Btn = () => {
   const textAreaValue = useSelector((state) => state.value);
+  function createTodos() {
+    if (!localStorage.getItem("todos")) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    }
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    todos.push({ value: textAreaValue, id: randomId(30, "aA0") });
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
 
   return (
     <div className={styles.btn}>
       <Button
         startDecorator={<Add />}
         color="neutral"
-        onClick={function () {
+        onClick={function (e) {
           console.log(textAreaValue);
+          createTodos();
+          console.log(!!localStorage.getItem("todos"));
         }}
       >
         ADD
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,5 +1,8 @@
 export const reducer = (state = {}, action) => {
   switch (action.type) {
+    case "UPDATE":
+      state = JSON.parse(localStorage.getItem("todos"));
+      return state;
     default:
       return state;
   }
