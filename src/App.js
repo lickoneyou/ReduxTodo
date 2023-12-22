7c5137a6d50f1bd21b3e96021475e238781bf431
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-12-22 10:27:37 +0300

    add main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
